efc49bf89c367736abaae99233b1a1a9b0c8113e
StNuralisa <150004163+StNuralisa@users.noreply.github.com>
2024-06-26 11:57:37 +0800

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,23 @@
+console.log("hello you");
+let nama = "ST.NURALISA";
+console.log("nama :" +nama)
+let daerah = "BULUKUMBA";
+console.log("daerah :" +daerah)
+let tanggallahir = "02-07-2004";
+console.log("tanggallahir :" +tanggallahir)
+let pekerjaan = "MAHASISWA";
+console.log("pekerjaan :" +pekerjaan)
+
+//Tugas 2
+//Buat umurnya = tahun sekarang - tahun lahir
+let umur = "19";
+let tahunlahir = "2004";
+let tahunsekarang = "2024";
+let umursaya = "tahunlahir : - tahunsekarang";
+console.log("umur :" + umur + "tahun");
+//tambahkan nama depannya jadi "bpk/ibu.nama"
+let namadepan = "ibu " + nama;
+console.log("namalengkap :" + namadepan);
+//Tambahkan asal daerahnya jadi "asaldaerah - indonesia"
+let asaldaerah = daerah + "(BULUKUMBA - INDONESIA)";
+console.log("asaldaerah :" + asaldaerah);
